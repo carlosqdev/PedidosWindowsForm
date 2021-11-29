913f34ae80b65d4e80aa5b7ed041073b558c9e79
carlosqdev <carlosquintero95nic@gmail.com>
2021-11-29 15:25:50 -0600

    Elimine form1, y cree el formulario para crear pedidos

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -16,7 +16,7 @@ namespace Pedidos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frm_CrearPedidos());
         }
     }
 }
--- Pedidos/frm_CrearPedidos.cs
+++ Pedidos/frm_CrearPedidos.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Pedidos
 {
-    public partial class Form1 : Form
+    public partial class frm_CrearPedidos : Form
     {
-        public Form1()
+        public frm_CrearPedidos()
         {
             InitializeComponent();
         }
